86b191cbd28886b7f1d4e97aa9bf25bdb7f42707
pawanpc-671 <pawanpradhanpc671@gmail.com>
2022-06-28 11:00:28 +0530

    finally done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import App from './App';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
-root.render(
-   <BrowserRouter>
-      <App />
-   </BrowserRouter>
-)
+root.render(<App />);
