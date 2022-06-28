7fe984b89e6a9f6a4bc34ab6fe7fb9a253beda73
pawanpc-671 <pawanpradhanpc671@gmail.com>
2022-06-28 10:58:08 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import React from 'react';
 import "./App.css";
 import ViewChapter from './pages/viewChapter/ViewChapter';
 
@@ -7,10 +6,7 @@ const App = () => {
 
    return (
       <div className='app-container'>
-         <Routes>
-            <Route path="/" element={<Navigate to="/chapter/:id/lesson/:id" />} />
-            <Route path="/chapter/:id/lesson/:id" element={<ViewChapter />} />
-         </Routes>
+         <ViewChapter />
       </div>
    )
 }
